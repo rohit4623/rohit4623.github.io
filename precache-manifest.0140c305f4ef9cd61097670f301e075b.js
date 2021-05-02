0e3f0f613a545798ee1360164fad531a230af216
rohit4623 <rohitkhole5@gmail.com>
2021-05-02 21:36:32 +0530

    Updates

---
+++ precache-manifest.0140c305f4ef9cd61097670f301e075b.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0255a4d57ef05b82d98ca8b9c7895bf",
+    "url": "/birthday/index.html"
+  },
+  {
+    "revision": "db3c4cfdc3126ac4008f",
+    "url": "/birthday/static/css/main.9cfec18b.chunk.css"
+  },
+  {
+    "revision": "71964326fa4d6f4027cb",
+    "url": "/birthday/static/js/2.556a1b20.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/birthday/static/js/2.556a1b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3c4cfdc3126ac4008f",
+    "url": "/birthday/static/js/main.fbe2c952.chunk.js"
+  },
+  {
+    "revision": "f19f5db21712150e969a",
+    "url": "/birthday/static/js/runtime-main.2fc9aa87.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/birthday/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/birthday/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/birthday/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/birthday/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/birthday/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d3b5d54ac0d0764ebc97b260fa47c135",
+    "url": "/birthday/static/media/cake-2.d3b5d54a.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/birthday/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/birthday/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/birthday/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/birthday/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/birthday/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/birthday/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/birthday/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/birthday/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/birthday/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/birthday/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/birthday/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/birthday/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "77ba37b9e3a414e4630dc49a7e4c4b65",
+    "url": "/birthday/static/media/img-1.77ba37b9.jpg"
+  },
+  {
+    "revision": "8d46d7274ca21b4f1909bd094393d332",
+    "url": "/birthday/static/media/img-10.8d46d727.jpg"
+  },
+  {
+    "revision": "51d7828200740d655aa64ab713b3c193",
+    "url": "/birthday/static/media/img-11.51d78282.jpg"
+  },
+  {
+    "revision": "b6ddd51e4abbfca1d741d7324cbbd820",
+    "url": "/birthday/static/media/img-12.b6ddd51e.jpg"
+  },
+  {
+    "revision": "edca00aad5d5489f09322e8cefddcf23",
+    "url": "/birthday/static/media/img-13.edca00aa.jpg"
+  },
+  {
+    "revision": "4ae3f34c05cb32dd83c4f40c689c9c82",
+    "url": "/birthday/static/media/img-14.4ae3f34c.jpg"
+  },
+  {
+    "revision": "eef9235d7df5207f6d6500148312f830",
+    "url": "/birthday/static/media/img-15.eef9235d.jpg"
+  },
+  {
+    "revision": "11ed0ddebc15525a64d6f46ec091e685",
+    "url": "/birthday/static/media/img-16.11ed0dde.jpg"
+  },
+  {
+    "revision": "77225840f3fd31892b1fba660d051dba",
+    "url": "/birthday/static/media/img-17.77225840.jpg"
+  },
+  {
+    "revision": "6f1d20b0904f66c32db00b0e819ff6be",
+    "url": "/birthday/static/media/img-18.6f1d20b0.jpg"
+  },
+  {
+    "revision": "c8dc2612a6f6f65668282f40e896017b",
+    "url": "/birthday/static/media/img-19.c8dc2612.jpg"
+  },
+  {
+    "revision": "e9dd613651d68b22353266ea78916642",
+    "url": "/birthday/static/media/img-2.e9dd6136.jpg"
+  },
+  {
+    "revision": "60756ae65d834d91525592745b5cb547",
+    "url": "/birthday/static/media/img-20.60756ae6.jpg"
+  },
+  {
+    "revision": "3a13e55f38ad0a303d91b82ffda5d8fe",
+    "url": "/birthday/static/media/img-21.3a13e55f.jpg"
+  },
+  {
+    "revision": "b4439075928ef22c318438b8fb29a313",
+    "url": "/birthday/static/media/img-22.b4439075.jpg"
+  },
+  {
+    "revision": "8ae28adb7bfa243b35e719ccd9a9c825",
+    "url": "/birthday/static/media/img-23.8ae28adb.jpg"
+  },
+  {
+    "revision": "440aa0c286808215bcbf99905bdebd58",
+    "url": "/birthday/static/media/img-24.440aa0c2.jpg"
+  },
+  {
+    "revision": "ac20ef452794075bad45d2f8c6972279",
+    "url": "/birthday/static/media/img-25.ac20ef45.jpg"
+  },
+  {
+    "revision": "24b8c695800a12fae7dd38f82196c46d",
+    "url": "/birthday/static/media/img-26.24b8c695.jpg"
+  },
+  {
+    "revision": "1b2373e809e74151e08a43a9c68d93c0",
+    "url": "/birthday/static/media/img-27.1b2373e8.jpg"
+  },
+  {
+    "revision": "1f7566c48039dec34508594ed5849bdc",
+    "url": "/birthday/static/media/img-28.1f7566c4.jpg"
+  },
+  {
+    "revision": "36ccf7ffc67aebcb4443be4ca8ae93b3",
+    "url": "/birthday/static/media/img-29.36ccf7ff.jpg"
+  },
+  {
+    "revision": "264ef9514431e39154b5613097fad3df",
+    "url": "/birthday/static/media/img-3.264ef951.jpg"
+  },
+  {
+    "revision": "79c0c3e8c41b6d884a56d1ced95b3dd0",
+    "url": "/birthday/static/media/img-30.79c0c3e8.jpg"
+  },
+  {
+    "revision": "ee04790c1ef146dc00d51112ad74b0e6",
+    "url": "/birthday/static/media/img-32.ee04790c.jpg"
+  },
+  {
+    "revision": "236e8965389c7346dd207cc89a99d92c",
+    "url": "/birthday/static/media/img-33.236e8965.jpg"
+  },
+  {
+    "revision": "73ac98008db092a1e908df025a0d8d17",
+    "url": "/birthday/static/media/img-34.73ac9800.jpg"
+  },
+  {
+    "revision": "f7df8a6d721f4a6c24230a72b6485ee1",
+    "url": "/birthday/static/media/img-35.f7df8a6d.jpg"
+  },
+  {
+    "revision": "077db4b97d6020bed781354bcd554901",
+    "url": "/birthday/static/media/img-4.077db4b9.jpg"
+  },
+  {
+    "revision": "a1ae2d18b36fe15bf357f44abb9caede",
+    "url": "/birthday/static/media/img-5.a1ae2d18.jpg"
+  },
+  {
+    "revision": "e69d222ef2d822d6826f7626913a5f09",
+    "url": "/birthday/static/media/img-6.e69d222e.jpg"
+  },
+  {
+    "revision": "2feb0f9a6724435fd55c0a85f370b92c",
+    "url": "/birthday/static/media/img-7.2feb0f9a.jpg"
+  },
+  {
+    "revision": "5b079bcd86f0982958adbac018c4553b",
+    "url": "/birthday/static/media/img-8.5b079bcd.jpg"
+  },
+  {
+    "revision": "b2e17aa444f61ff28be2784639f78ec4",
+    "url": "/birthday/static/media/img-9.b2e17aa4.jpg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/birthday/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/birthday/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/birthday/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/birthday/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/birthday/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/birthday/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
